534f2a2c62ee498e1c7e4fdb9d8463657eb1bd6e
VladLich <yashka22855@mail.ru>
2022-01-12 17:17:20 +0300

    initial commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function plus(){
-  var num1, num2, result;
+  let num1, num2, result;
   num1 = document.getElementById('n1').value;
   num1 = parseInt(num1);
   num2 = document.getElementById('n2').value;
@@ -10,7 +10,7 @@ function plus(){
 }
 
 function minus(){
-  var num1, num2, result;
+  let num1, num2, result;
   num1 = document.getElementById('n1').value;
   num1 = parseInt(num1);
   num2 = document.getElementById('n2').value;
@@ -20,7 +20,7 @@ function minus(){
 }
 
 function multiply(){
-  var num1, num2, result;
+  let num1, num2, result;
   num1 = document.getElementById('n1').value;
   num1 = parseInt(num1);
   num2 = document.getElementById('n2').value;
@@ -30,7 +30,7 @@ function multiply(){
 }
 
 function divide(){
-  var num1, num2, result;
+  let num1, num2, result;
   num1 = document.getElementById('n1').value;
   num1 = parseInt(num1);
   num2 = document.getElementById('n2').value;
@@ -42,11 +42,17 @@ function divide(){
 let A = 'a';
   let S = 's';
 function binaryAgent(str) {
-    let array = str.split(" ");
-    return array.map(code => String.fromCharCode(parseInt(code, 2))).join("");
+    let array = str.split();
+    return array.map(code => String.fromCharCode(parseInt(code, 2))).join();
   }
   
   console.log(A +  binaryAgent("01001110") + A + binaryAgent("01001110") + A + S);
+
+function min(a, b, c){
+  return a - b - c;
+}
+console.log(min(10, 5, 3));
+
  
 
   
